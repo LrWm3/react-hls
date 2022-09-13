22caba21dec90e204a83a7d02507389098114a76
Marcos Alberto <marcos@marcosalberto.com.br>
2022-09-13 19:00:18 -0300

    Fix Config typescript interface

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, RefObject } from 'react';
-import Hls, { Config } from 'hls.js';
+import Hls, { HlsConfig } from 'hls.js';
 
 export interface HlsPlayerProps
   extends React.VideoHTMLAttributes<HTMLVideoElement> {
-  hlsConfig?: Config;
+  hlsConfig?: HlsConfig;
   playerRef: RefObject<HTMLVideoElement>;
   src: string;
 }
